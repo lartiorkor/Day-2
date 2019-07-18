9136e324e6b35ce7f1231ee10f1a748a6e999dda
lartiorkor <coloneltiorkor@gmail.com>
2019-07-18 11:20:29 +0000

    my first commit

---
+++ day2.py
@@ -0,0 +1,23 @@
+print('"I am a student", said the boy.')
+
+print("This is a girl's bag.")
+
+print('''How
+are
+you''')
+
+len("hello")
+
+print('hello,' + 'Josh')
+
+x='have a'
+y='good day'
+print(x, y)
+
+print('zing')
+
+input('How old are you?')
+
+hello = 8
+hello*2
+
